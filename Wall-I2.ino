8c8a0273d3e7efd762b2ade665617a84b26f6278
VaroWz <antoine.bouille.24@gmail.com>
2024-04-04 09:02:38 +0200

    Update Wall-I2.ino

--- Wall-I2.ino
+++ Wall-I2.ino
@@ -6,40 +6,52 @@ VL6180X capteurD;
 VL6180X capteur;
 VL6180X capteurG;
 
-/*#define IR_DEVANT A0
-#define IR_DROITE A1
-#define IR_GAUCHE A2*/
-
-//Servo monServo;
-//Servo monServo2;
+Servo monServo;
+Servo monServo2;
 
 void setup() {
 
   Wire.begin();
 
-  //monServo.attach(8);
-  //monServo2.attach(9);
+  monServo.attach(8);
+  monServo2.attach(9);
 
   Serial.begin(9600);
 
-  initCapteurs(capteurD, 0x20);
-  initCapteurs(capteur, 0x22);
-  initCapteurs(capteurG, 0x24);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
 
-  //digitalWrite(12, HIGH);
-  //capteurD.init();
-  //capteurD.configureDefault();
-  //capteurD.setAddress(0x20);
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
 
-  //digitalWrite(11, HIGH);
-  //capteur.init();
-  //capteur.configureDefault();
-  //capteurD.setAddress(0x22);
+  digitalWrite(10, HIGH);             //DROITE
+  delay(150);
+  capteurG.init();
+  delay(150);
+  capteurG.setAddress(0x20);
+  delay(150);
+  capteurG.configureDefault();
+  delay(150);
 
-  //digitalWrite(10, HIGH);
-  //capteurG.init();
-  //capteurG.configureDefault();
-  //capteurD.setAddress(0x24);
+  digitalWrite(12, HIGH);             //GAUCHE
+  delay(150);
+  capteurD.init();
+  delay(150);
+  capteurD.setAddress(0x22);
+  delay(150);
+  capteurD.configureDefault();
+  delay(150);
+
+  digitalWrite(11, HIGH);           //MILIEU
+  delay(150);
+  capteur.init();
+  delay(150);
+  capteur.setAddress(0x24);
+  delay(150);
+  capteur.configureDefault();
+  delay(150);
 
 
 
@@ -50,35 +62,40 @@ void initCapteurs(VL6180X& cap, uint8_t addresse) {
   cap.configureDefault();
   cap.setTimeout(500);
 }
-/*
+
 void StopMoteur(){
+  Serial.println("STOP");
   monServo.writeMicroseconds(1500);
   monServo2.writeMicroseconds(1450);
 }
 
 void AvanceMoteur(){
+  Serial.println("AVANCE");
   monServo.writeMicroseconds(2000);
   monServo2.writeMicroseconds(1000);
 }
 
 void ReculerMoteur(){
+  Serial.println("RECULE");
   monServo.writeMicroseconds(1000);
   monServo2.writeMicroseconds(2000);
+  delay(100);
 }
 
 void TourneG(){
-  monServo.writeMicroseconds(1000);
+  Serial.println("GAUCHE");
+  monServo.writeMicroseconds(1500);
   monServo2.writeMicroseconds(1000);
 }
 
 void TourneD(){
+  Serial.println("DROITE");
   monServo.writeMicroseconds(2000);
-  monServo2.writeMicroseconds(2000);
-}*/
+  monServo2.writeMicroseconds(1500);
+}
 
 void loop() {
   
-  //Serial.println("test");
 
   int DistanceDroite = capteurD.readRangeSingle();
   int DistanceDevant = capteur.readRangeSingle();
@@ -96,47 +113,50 @@ void loop() {
   Serial.print("Capteur gauche: ");
   Serial.println(DistanceGauche);
 
-  /*int DistanceDevant = analogRead(IR_DEVANT);
-  int DistanceDroite = analogRead(IR_DROITE);
-  int DistanceGauche = analogRead(IR_GAUCHE);
+  
+  if(DistanceDevant > 200 && DistanceDroite > 200 && DistanceGauche > 200){
 
-  if(DistanceDevant > 150){
     AvanceMoteur();
-    delay(300);
 
-    //rectifs
-    if(DistanceGauche < 15){
-      TourneD();
-      delay(25);
-    }
-    else if (DistanceDroite < 15) {
-      TourneG();
-      delay(25);
-    }
   }
   else{
-    if (DistanceDevant<50){
-      ReculerMoteur();
-      delay(100);
-      TourneG();
-      delay(100);
+    if(DistanceDevant > 130){
+      AvanceMoteur();
+      /*if(DistanceDroite > 35 && DistanceGauche > 35){
+        AvanceMoteur();
+      }
+      
+    
+      //rectifs
+      if(DistanceGauche < 35){
+        TourneD();
+      }
+      else if (DistanceDroite < 35) {
+        TourneG();
+      }*/
     }
-    //Un peu trop à droite
-    else if (DistanceDroite<DistanceGauche) {
-      TourneG();
-      delay(100);
+    else{
+      if (DistanceDevant<130){
+        if(DistanceDevant < 30){
+          ReculerMoteur();
+        }
+        
+        
+        else if (DistanceDroite<DistanceGauche) {
+          TourneG();
 
+        }
+      //Un peu trop à gauche
+        else if (DistanceGauche<DistanceDroite) {
+          TourneD();
+
+        }
+        //TourneG();
+      }
+      //Un peu trop à droite
+      
     }
-    //Un peu trop à gauche
-    else if (DistanceGauche<DistanceDroite) {
-      TourneD();
-      delay(100);
-
-    }
-  }*/
-
-
 
   
-
+  }
 }
